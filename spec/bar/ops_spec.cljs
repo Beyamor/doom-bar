c72e465d72aa6ad16f05ca580507a907e531ddc1
beyamor <beyamor@gmail.com>
2014-05-10 14:06:37 -0700

    Using set-registers some more

--- spec/bar/ops_spec.cljs
+++ spec/bar/ops_spec.cljs
@@ -5,27 +5,27 @@
             [bar.registers :as registers]
             [bar.bit :as bit]
             [bar.ops :as ops]
-            [bar.system :as system :refer [set-register]]))
+            [bar.system :as system :refer [set-registers]]))
 
 (describe "addr-e"
           (it "should add registers a and e"
               (should= 4
                        (-> system/zeroed
-                           (set-register :e 4)
+                           (set-registers :e 4)
                            (ops/execute ops/addr-e)
                            :registers :a)))
 
           (it "should set the time taken"
               (let [registers (-> system/zeroed
-                                  (set-register :e 4)
+                                  (set-registers :e 4)
                                   (ops/execute ops/addr-e)
                                   :registers)]
                 (should= 1 (:m registers))
                 (should= 4 (:t registers))))
 
           (let [overflown-registers (-> system/zeroed
-                                        (set-register :a 255)
-                                        (set-register :e 5)
+                                        (set-registers :a 255
+                                                       :e 5)
                                         (ops/execute ops/addr-e)
                                         :registers)]
             (it "should truncate to 8 bits"
@@ -36,15 +36,15 @@
 
           (it "should set the half-carry bit"
               (should (-> system/zeroed
-                          (set-register :a 0x8)
-                          (set-register :e 0x8)
+                          (set-registers :a 0x8
+                                         :e 0x8)
                           (ops/execute ops/addr-e)
                           :registers
                           (registers/flag-set? :half-carry)))
 
               (should-not (-> system/zeroed
-                              (set-register :a 0x7)
-                              (set-register :e 0x8)
+                              (set-registers :a 0x7
+                                             :e 0x8)
                               (ops/execute ops/addr-e)
                               :registers
                               (registers/flag-set? :half-carry))))
@@ -56,8 +56,8 @@
                           (registers/flag-set? :zero)))
 
               (should (-> registers/zeroed
-                          (set-register :a 0x1)
-                          (set-register :e 0xff)
+                          (set-registers :a 0x1
+                                         :e 0xff)
                           (ops/execute ops/addr-e)
                           :registers
                           (registers/flag-set? :zero)))))
