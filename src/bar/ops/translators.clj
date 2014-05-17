d8722ebdcdef18f8e8ccc30f78e4a48720de0b10
beyamor <beyamor@gmail.com>
2014-05-17 13:15:10 -0700

    Whoops, symbol literals in the translators

--- src/bar/ops/translators.clj
+++ src/bar/ops/translators.clj
@@ -14,11 +14,11 @@
   [arg1 arg2]
   (let [is-list? (list? arg1)]
     (match [is-list? arg1 arg2]
-           [true _ a]
+           [true _ 'A]
            (let [[r1 r2] (symbol->keywords (first arg1))]
              `(bar.ops/store-from-registers-address ~r1 ~r2))
 
-           [false _ d16]
+           [false _ 'd16]
            (let [[r1 r2] (symbol->keywords arg1)]
              `(bar.ops/load-to-registers ~r1 ~r2)))))
 
