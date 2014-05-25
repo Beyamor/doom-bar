f0a6540494fd576f78e12b5b40d1046bad78a5fe
beyamor <beyamor@gmail.com>
2014-05-25 12:40:30 -0700

    No more `let` in the translators specs

--- spec/bar/ops/translators_spec.cljs
+++ spec/bar/ops/translators_spec.cljs
@@ -10,110 +10,105 @@
             [bar.system :as system]))
 
 (describe "LD"
+          (with registers (-> system/zeroed
+                              (->/in [:memory]
+                                     (memory/store 0 2)
+                                     (memory/store 1 3))
+                              (ops/execute (LD BC, d16))
+                              :registers))
           (it "should handle the load-to-registers form"
-              (let [op (LD BC, d16)
-                    registers (-> system/zeroed
-                                  (->/in [:memory]
-                                         (memory/store 0 2)
-                                         (memory/store 1 3))
-                                  (ops/execute op)
-                                  :registers)]
-                (should= 2 (registers :c))
-                (should= 3 (registers :b))
-                (should= 3 (registers :m))))
+              (should= 2 (@registers :c))
+              (should= 3 (@registers :b))
+              (should= 3 (@registers :m)))
 
-          (it "should handle the store-from-registers-address form"
-              (let [op (LD (BC), A)
-                    memory (-> system/zeroed
-                               (->/in [:registers]
-                                      (assoc :a 23
-                                             :b 9
-                                             :c 8))
-                               (ops/execute op)
-                               :memory)]
-                (should= 23 (memory/load memory 0x0908))))
-
-          (it "should handle the load-immediate-value form"
-              (let [registers (-> system/zeroed
-                                  (->/in [:memory]
-                                         (memory/store 0 2))
-                                  (ops/execute (ops/load-immediate-value :b))
-                                  :registers)]
-                (should= 2 (registers :b))
-                (should= 0x001 (registers :pc))))
-          
           (with memory (-> system/zeroed
-                           (assoc-in [:registers :sp] 0xfff8)
-                           (->/in [:memory]
-                                  (memory/store 0 0x00)
-                                  (memory/store 1 0xc1))
-                           (ops/execute (LD (a16), SP))
+                           (->/in [:registers]
+                                  (assoc :a 23
+                                         :b 9
+                                         :c 8))
+                           (ops/execute (LD (BC), A))
                            :memory))
+          (it "should handle the store-from-registers-address form"
+              (should= 23 (memory/load @memory 0x0908)))
+
+          (with registers2 (-> system/zeroed
+                               (->/in [:memory]
+                                      (memory/store 0 2))
+                               (ops/execute (ops/load-immediate-value :b))
+                               :registers))
+          (it "should handle the load-immediate-value form"
+              (should= 2 (@registers2 :b))
+              (should= 0x001 (@registers2 :pc)))
+
+          (with memory2 (-> system/zeroed
+                            (assoc-in [:registers :sp] 0xfff8)
+                            (->/in [:memory]
+                                   (memory/store 0 0x00)
+                                   (memory/store 1 0xc1))
+                            (ops/execute (LD (a16), SP))
+                            :memory))
           (it "should handle the store-stack-pointer form"
-              (should= 0xf8 (memory/load @memory 0xc100))
-              (should= 0xff (memory/load @memory 0xc101))))
+              (should= 0xf8 (memory/load @memory2 0xc100))
+              (should= 0xff (memory/load @memory2 0xc101))))
 
 
 (describe "INC"
           (it "should handle the increment-registers-address form"
-              (let [op (INC BC)]
-                (should= 2 (-> system/zeroed
-                               (->/in [:registers]
-                                      (assoc :b 9
-                                             :c 8))
-                               (->/in [:memory]
-                                      (memory/store 0x0908 1))
-                               (ops/execute op)
-                               :memory
-                               (memory/load 0x0908)))
+              (should= 2 (-> system/zeroed
+                             (->/in [:registers]
+                                    (assoc :b 9
+                                           :c 8))
+                             (->/in [:memory]
+                                    (memory/store 0x0908 1))
+                             (ops/execute (INC BC))
+                             :memory
+                             (memory/load 0x0908)))
 
-                (should= 0 (-> system/zeroed
-                               (->/in [:registers]
-                                      (assoc :b 9
-                                             :c 8))
-                               (->/in [:memory]
-                                      (memory/store 0x0908 0xff))
-                               (ops/execute op)
-                               :memory
-                               (memory/load 0x0908)))))
+              (should= 0 (-> system/zeroed
+                             (->/in [:registers]
+                                    (assoc :b 9
+                                           :c 8))
+                             (->/in [:memory]
+                                    (memory/store 0x0908 0xff))
+                             (ops/execute (INC BC))
+                             :memory
+                             (memory/load 0x0908))))
 
           (it "should handle the increment-register form"
-              (let [op (INC B)]
-                (should= 2 (-> system/zeroed
-                               (->/in [:registers]
-                                      (assoc :b 1))
-                               (ops/execute op)
-                               :registers :b))
-                (should= 0 (-> system/zeroed
-                               (->/in [:registers]
-                                      (assoc :b 0xff))
-                               (ops/execute op)
-                               :registers :b)))))
+              (should= 2 (-> system/zeroed
+                             (->/in [:registers]
+                                    (assoc :b 1))
+                             (ops/execute (INC B))
+                             :registers :b))
+              (should= 0 (-> system/zeroed
+                             (->/in [:registers]
+                                    (assoc :b 0xff))
+                             (ops/execute (INC B))
+                             :registers :b))))
 
 (describe "DEC"
           (it "should handle the decrement-register form"
-              (let [op (DEC B)]
-                (should= 1 (-> system/zeroed
-                               (->/in [:registers]
-                                      (assoc :b 2))
-                               (ops/execute op)
-                               :registers :b))
-                (should= 0xff (-> system/zeroed
-                                  (->/in [:registers]
-                                         (assoc :b 0))
-                                  (ops/execute op)
-                                  :registers :b)))))
+              (should= 1 (-> system/zeroed
+                             (->/in [:registers]
+                                    (assoc :b 2))
+                             (ops/execute (DEC B))
+                             :registers :b))
+              (should= 0xff (-> system/zeroed
+                                (->/in [:registers]
+                                       (assoc :b 0))
+                                (ops/execute (DEC B))
+                                :registers :b))))
 
 (describe "ADD"
           (with registers (-> system/zeroed
-                               (->/in [:registers]
-                                      (assoc :h 0x8a
-                                             :l 0x23
-                                             :b 0x06
-                                             :c 0x05)
-                                      (registers/unset-flag :operation))
-                               (ops/execute (ADD HL, BC))
-                               :registers))
+                              (->/in [:registers]
+                                     (assoc :h 0x8a
+                                            :l 0x23
+                                            :b 0x06
+                                            :c 0x05)
+                                     (registers/unset-flag :operation))
+                              (ops/execute (ADD HL, BC))
+                              :registers))
           (it "should handle the add-register-words form"
               (should= 0x90 (@registers :h))
               (should= 0x28 (@registers :l))))
